cb45a6f2cb33dd05cae7db2f0fc73c82f69625ec
aniket <aniket.howrah@gmail.com>
2021-03-22 18:40:14 +0530

    First_Upload

---
+++ zero-restore/subjs.js
@@ -0,0 +1,15 @@
+function emailProtection() {
+    var part2 = "kar";
+    var part1 = "aupendu";
+    var part3 = Math.pow(2, 6);
+    var part6 = ".ac.in";
+    var part5 = "iitkgp";
+  
+    var x = confirm("Click OK to email me.");
+  
+    if (x == true) {
+      window.open(
+        "mailto:" + part1 + part2 + String.fromCharCode(part3) + part5 + part6
+      );
+    }
+  }
