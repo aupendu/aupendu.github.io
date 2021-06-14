315f1a48178872a6d14f325b83ca6bf33ed89224
aniket <aniket.howrah@gmail.com>
2021-06-14 12:22:09 +0530

    Jquery added
    Some hover effects and email section updated using jqeury

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-document.getElementById("yearContainer").innerHTML = new Date().getFullYear();
+$(document).ready(
+    $("#mailTo").click(function () {
+        var part2 = "kar";
+        var part1 = "aupendu";
+        var part3 = Math.pow(2, 6);
+        var part6 = ".ac.in";
+        var part5 = "iitkgp";
+
+        var x = confirm(
+            part1 + part2 + String.fromCharCode(part3) + part5 + part6 + "\n\nClick \"OK\" to open your default email app."
+        );
+        if (x == true)
+            window.location.href = "mailto:" + part1 + part2 + String.fromCharCode(part3) + part5 + part6;
+
+
+    }),
+    $(".gScholar").mouseover(function () {
+
+        $(this).css("cursor", "pointer");
+        $(this).css("color", "#527bbd");
+        $(this).css("font-weight", "bold");
+
+    }),
+    $(".gScholar").mouseleave(function () {
+
+        $(this).css("color", "gray");
+        $(this).css("font-weight", "normal");
+        $(this).css("cursor", "context-menu");
+
+    })
+);
