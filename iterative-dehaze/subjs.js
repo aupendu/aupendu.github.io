d59b38712ace203ab6bf544f97003cf589b440f9
aniket <aniket.howrah@gmail.com>
2021-06-19 08:51:27 +0530

    Update to BS5 and others
    moving script, fallback to cdn, centering, underline name

--- iterative-dehaze/subjs.js
+++ iterative-dehaze/subjs.js
@@ -1,15 +1,16 @@
-function emailProtection() {
-  var part2 = "kar";
-  var part1 = "aupendu";
-  var part3 = Math.pow(2, 6);
-  var part6 = ".ac.in";
-  var part5 = "iitkgp";
+$(document).ready(
+  $("#mailTo").click(function () {
+    var part2 = "kar";
+    var part1 = "aupendu";
+    var part3 = Math.pow(2, 6);
+    var part6 = ".ac.in";
+    var part5 = "iitkgp";
 
-  var x = confirm("Click OK to email me.");
-
-  if (x == true) {
-    window.open(
-      "mailto:" + part1 + part2 + String.fromCharCode(part3) + part5 + part6
+    var x = confirm(
+      part1 + part2 + String.fromCharCode(part3) + part5 + part6 + "\n\nClick \"OK\" to open your default email app."
     );
-  }
-}
+    if (x == true)
+      window.location.href = "mailto:" + part1 + part2 + String.fromCharCode(part3) + part5 + part6;
+
+  }),
+  $("#mailTo").css("cursor", "pointer"));
