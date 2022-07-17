a2135fdcadc9f74d6dee89c12239e762a8b6217f
aniket-patra <aniket.howrah@gmail.com>
2022-07-17 10:05:28 +0530

    added thumb image for lfsafa

---
+++ lfsafa-sr/subjs.js
@@ -0,0 +1,16 @@
+$(document).ready(
+    $("#mailTo").click(function () {
+      var part2 = "kar";
+      var part1 = "aupendu";
+      var part3 = Math.pow(2, 6);
+      var part6 = ".ac.in";
+      var part5 = "iitkgp";
+  
+      var x = confirm(
+        part1 + part2 + String.fromCharCode(part3) + part5 + part6 + "\n\nClick \"OK\" to open your default email app."
+      );
+      if (x == true)
+        window.location.href = "mailto:" + part1 + part2 + String.fromCharCode(part3) + part5 + part6;
+  
+    }),
+    $("#mailTo").css("cursor", "pointer"));
